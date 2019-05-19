362e039706203f5bd1a758d0558b1e274c54697b
Renan Machado <renan.mav@hotmail.com>
2019-05-19 15:55:00 -0300

    Add CstMove

--- CST-MATLAB-API-master/Utils/DrawSquareSRR.m
+++ CST-MATLAB-API-master/Utils/DrawSquareSRR.m
@@ -1,4 +1,4 @@
-function DrawSquareSRR(mws, W, W0, d, t, h, h0, material, id)
+function DrawSquareSRR(mws, W, W0, d, t, h, h0, material, id, offset, distance)
 %
 % Draw a Square SRR unique cell.
 %
@@ -12,40 +12,40 @@ function DrawSquareSRR(mws, W, W0, d, t, h, h0, material, id)
 % @param {int} [id] Cell identifier
 
 % Draw outside ring
-Xrange = [-W/2 W/2];
-Yrange = [-W/2 W/2];
+Xrange = [-W/2+offset W/2+offset];
+Yrange = [-W/2+distance W/2+distance];
 Zrange = [h h+h0];
 Cstbrick(mws, strcat('Outring-',num2str(id)), 'component1', material, Xrange, Yrange, Zrange);
 
 % Draw cutting of outside ring
-Xrange = [-W/2+t W/2-t];
-Yrange = [-W/2+t W/2-t];
+Xrange = [-W/2+t+offset W/2-t+offset];
+Yrange = [-W/2+t+distance W/2-t+distance];
 Cstbrick(mws, 'Empty', 'component1', material, Xrange, Yrange, Zrange);
 
 CstSubtract(mws, strcat('component1:Outring-',num2str(id)), 'component1:Empty');
 
 % Draw gap of outside ring
-Xrange = [-W/2 -W/2+t];
-Yrange = [-W0/2 W0/2];
+Xrange = [-W/2+offset -W/2+t+offset];
+Yrange = [-W0/2+distance W0/2+distance];
 Cstbrick(mws, 'Empty', 'component1', material, Xrange, Yrange, Zrange);
 
 CstSubtract(mws, strcat('component1:Outring-',num2str(id)), 'component1:Empty');
 
 % Draw inside ring
-Xrange = [-W/2+t+d W/2-t-d];
-Yrange = [-W/2+t+d W/2-t-d];
+Xrange = [-W/2+t+d+offset W/2-t-d+offset];
+Yrange = [-W/2+t+d+distance W/2-t-d+distance];
 Cstbrick(mws, strcat('Inring-',num2str(id)), 'component1', material, Xrange, Yrange, Zrange);
 
 % Draw cutting of inside ring
-Xrange = [-W/2+2*t+d W/2-2*t-d];
-Yrange = [-W/2+2*t+d W/2-2*t-d];
+Xrange = [-W/2+2*t+d+offset W/2-2*t-d+offset];
+Yrange = [-W/2+2*t+d+distance W/2-2*t-d+distance];
 Cstbrick(mws, 'Empty', 'component1', material, Xrange, Yrange, Zrange);
 
 CstSubtract(mws, strcat('component1:Inring-',num2str(id)), 'component1:Empty');
 
 % Draw gap of inside ring
-Xrange = [W/2-2*t-d W/2-t-d];
-Yrange = [-W0/2 W0/2];
+Xrange = [W/2-2*t-d+offset W/2-t-d+offset];
+Yrange = [-W0/2+distance W0/2+distance];
 Cstbrick(mws, 'Empty', 'component1', material, Xrange, Yrange, Zrange);
 
 CstSubtract(mws, strcat('component1:Inring-',num2str(id)), 'component1:Empty');
